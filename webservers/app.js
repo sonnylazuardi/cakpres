df8f9a09fcde629a04dd28a2c480ce04a7712dbe
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-06-09 20:47:53 +0700

    nyonte

--- webservers/app.js
+++ webservers/app.js
@@ -26,14 +26,73 @@ var users = [];
 var onlineUsers = [];
 var answerers = {};
 
+var candidate_json = {};
+
 var calon_set = ['hr', 'jk', 'jw', 'ps'];
+var jenis_set = [
+    {
+        name: 'partai',
+        field: 'nama',
+        title: '',
+        category: 3
+    }, 
+    {
+        name: 'riwayat_pendidikan',
+        field: 'ringkasan',
+        title: '',
+        category: 1
+    }, 
+    {
+        name: 'riwayat_pekerjaan',
+        field: 'ringkasan',
+        title: '',
+        category: 1
+    },
+    {
+        name: 'nama_pasangan',
+        field: '',
+        title: 'Nama Pasangan',
+        category: 2
+    },
+    {
+        name: 'riwayat_organisasi',
+        field: 'ringkasan', 
+        title: '',
+        category: 1
+    },
+    {
+        name: 'riwayat_penghargaan',
+        field: 'ringkasan', 
+        title: '',
+        category: 1
+    },
+    {
+        name: 'tempat_lahir',
+        field: '', 
+        title: 'Tempat Lahir',
+        category: 2
+    },
+    {
+        name: 'tanggal_lahir',
+        field: '', 
+        title: 'Tanggal Lahir',
+        category: 2
+    },
+    {
+        name: 'jumlah_anak',
+        field: '', 
+        title: 'Jumlah Anak',
+        category: 2
+    }
+];
 
 var idcalon = 0;
 var idjenis = 0;
 var idpertanyaan = 0;
 
+var soal = '';
 var jawab = '';
-var counter = 10;
+var counter = 5;
 var answerPosition = 1;
 
 
@@ -46,9 +105,10 @@ app.get('/', function(req, res) {
 	res.send('Cakpres API v.0.0.1!')
 });
 
-app.get('/api/test', function(req, res) {
+app.get('/api/refresh', function(req, res) {
     request("http://api.pemiluapi.org/calonpresiden/api/caleg?apiKey=fea6f7d9ec0b31e256a673114792cb17", function(error, response, body) {
-      res.send(body);
+      candidate_json = JSON.parse(body);
+      res.send('ok!');
     });
 });
 
@@ -121,13 +181,33 @@ function timer() {
     setTimeout(function () {
         counter--;
         if (counter < 0) {
-            idcalon = Math.floor((Math.random()*calon_set.length-1)+1);
-
-            jawab = songs[soal].artist;
-            counter = 10;
+            
+            var hasil = candidate_json;
+            if (hasil.data) {
+                idcalon = Math.floor((Math.random()*calon_set.length-1)+1);
+                idjenis = Math.floor((Math.random()*jenis_set.length-1)+1);
+                // console.log(idjenis);
+                var calon = _.findWhere(hasil.data.results.caleg, {id: calon_set[idcalon]});
+                console.log(calon.nama);
+                console.log(jenis_set[idjenis].name);
+                if (jenis_set[idjenis].category == 1) {
+                    idpertanyaan = Math.floor((Math.random()*calon[jenis_set[idjenis].name].length-1)+1);
+                    soal = calon[jenis_set[idjenis].name][idpertanyaan][jenis_set[idjenis].field];
+                    console.log(soal);
+                } else if (jenis_set[idjenis].category == 2) {
+                    soal = jenis_set[idjenis].title + ': ';
+                    soal += calon[jenis_set[idjenis].name];
+                    console.log(soal);
+                } else if (jenis_set[idjenis].category == 3) {
+                    soal = calon[jenis_set[idjenis].name][jenis_set[idjenis].field];
+                    console.log(soal);
+                }
+            }
+               
+            counter = 5;
             answerPosition = 1;
             answerers = {};
-            io.sockets.emit('soal', {id: soal, hint: songs[soal].title, soal: acakKata(jawab), counter: counter});
+            // io.sockets.emit('soal', {id: soal, hint: songs[soal].title, soal: acakKata(jawab), counter: counter});
         }
         timer();
     }, 1000);
